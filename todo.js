2fb85151f447941b5e59c11d3c85c9400f4a3d0b
hemanth-2801 <113626456+hemanth-2801@users.noreply.github.com>
2022-11-21 22:23:42 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,60 @@
+
+const todoList = () => {
+    // eslint-disable-next-line no-undef
+    all = [];
+    const add = (todoItem) => {
+      // eslint-disable-next-line no-undef
+      all.push(todoItem);
+    };
+    const markAsComplete = (index) => {
+      // eslint-disable-next-line no-undef
+      all[index].completed = true;
+    };
+    let myDate = new Date();
+    let todayDate = myDate.toISOString().split("T")[0];
+    const overdue = () => {
+      // eslint-disable-next-line no-undef
+      let overduearray = all.filter(
+        (task) => Date.parse(task.dueDate) < Date.parse(todayDate)
+      );
+      return overduearray;
+    };
+  
+    const dueToday = () => {
+      // eslint-disable-next-line no-undef
+      let td = all.filter((task) => task.dueDate === todayDate);
+      return td;
+    };
+  
+    const dueLater = () => {
+      // eslint-disable-next-line no-undef
+      let duelaterarray = all.filter(
+        (task) => Date.parse(task.dueDate) > Date.parse(todayDate)
+      );
+      return duelaterarray;
+    };
+  
+    const toDisplayableList = (list) => {
+      let displayarray = list.map((work) => {
+        let workstatus = work.completed ? "x" : " ";
+        if (work.dueDate === todayDate) {
+          return `[${workstatus}] ${work.title}`;
+        } else {
+          return `[${workstatus}] ${work.title} ${work.dueDate}`;
+        }
+      });
+      return displayarray.join("\n");
+    };
+  
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+      toDisplayableList,
+    };
+  };
+  
+  module.exports = todoList;
